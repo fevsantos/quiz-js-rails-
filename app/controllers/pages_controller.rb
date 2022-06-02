7fbd9da7c3fa4af3774a0c87ab5a75b1eb3d779a
Fernanda Santos <fevsantos@gmail.com>
2022-06-02 00:36:59 -0300

    criando texto pra variavel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,10 @@ class PagesController < ApplicationController
   end
 
   def test
-    @questionnaire = Questionnaire.find(15)
+    @questionnaire = Questionnaire.find(16)
     @questions = []
     @options = []
-    @respostas = 0
+    @resposta = []
     @questionnaire.questions.each do |q|
       @questions << q.name
     end
@@ -22,17 +22,27 @@ class PagesController < ApplicationController
         @options << a.name
       end
     end
-      @questionnaire.questions.each do |q|
-        q.answers.each do |a|
-          if a.correct == true
-            @resposta = a.name
-          end
-          @resposta
-       end
+    @questionnaire.questions.each do |q|
+      q.answers.each do |a|
+        if a.correct == true
+          @resposta << a.name
+        end
+      end
     end
-    @primeira_opção = @options[0]
-    @segunda_opção = @options[1]
-    @terceira_opção = @options[2]
-  end
+    @n = @questions.length
+    @numeros = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
+    @loop = @numeros[0..@n]
+    @o = @options.length
+    @loop2 = @numeros[0..@o]
+    @loop2.each do |n|
+      instance_variable_set "@op#{n}".to_sym, @options[n]
+    end
+    # @primeira_opção = @options[0]
+    # @segunda_opção = @options[1]
+    # @terceira_opção = @options[2]
+    # @quarta_opção = @options[3]
+    # @quinta_opção = @options[4]
+    # @sexta_opção = @options[5]
 
+  end
 end
